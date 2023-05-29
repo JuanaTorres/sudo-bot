59c3fe7e6f0323284a74bc2c4264f5c5a7ec2cac
Juan Castillo <cfuendesign@tutanota.com>
2023-05-29 14:37:10 -0500

    :bug: fix bot name in prompt

--- commands.js
+++ commands.js
@@ -79,9 +79,9 @@ const setupCommands = async (botIO, estamos) => {
         response = await botIO._openai.createCompletion({
           model: "text-davinci-003",
           prompt: `
-        Eres "ValkaBot", un bot de Discord que interactúa con IA para asistir a CFuen, un VTuber que necesita consejos de programación de vez en cuando.
+        Eres "Sudo", un bot de Discord que interactúa con IA para asistir al GPC (Grupo de Programación Competitiva) de la Universidad El Bosque.
 
-ValkaBot, ahora mismo requerimos de tu ayuda, pues se ha invocado el comando \`valkabot -gpt --explain\`, para el cual el usuario espera una respuesta simple de no más de dos párrafos, que se sienta conversacional y que de la ilusión de que la redactó un compañero ingeniero. Este es el formato que utilizarás para resolver la tarea:
+Sudo, ahora mismo requerimos de tu ayuda, pues se ha invocado el comando \`sudo gpt --explain\`, para el cual el usuario espera una respuesta simple de no más de dos párrafos, que se sienta conversacional y que de la ilusión de que la redactó un compañero ingeniero. Este es el formato que utilizarás para resolver la tarea:
 
 ---
 
@@ -107,9 +107,9 @@ La solicitud que se te hizo y para la cual deberás generar un JSON, siguiendo e
         response = await botIO._openai.createCompletion({
           model: "text-davinci-003",
           prompt: `
-          Eres "ValkaBot", un bot de Discord que interactúa con IA para asistir a CFuen, un VTuber que necesita consejos de programación de vez en cuando.
+          Eres "Sudo", un bot de Discord que interactúa con IA para asistir al GPC (Grupo de Programación Competitiva) de la Universidad El Bosque.
 
-ValkaBot, ahora mismo requerimos de tu ayuda, pues se ha invocado el comando \`valkabot -gpt --doc\`, para el cual el usuario espera una respuesta cuya calidad sea equiparable a la de la documentación de un lenguaje, una librería y/o demás tecnologías de ingeniería de software. Este es el formato que utilizarás para resolver la tarea:
+Sudo, ahora mismo requerimos de tu ayuda, pues se ha invocado el comando \`sudo gpt --doc\`, para el cual el usuario espera una respuesta cuya calidad sea equiparable a la de la documentación de un lenguaje, una librería y/o demás tecnologías de ingeniería de software. Este es el formato que utilizarás para resolver la tarea:
 
 \`\`\`json
 {
@@ -128,9 +128,10 @@ ValkaBot, ahora mismo requerimos de tu ayuda, pues se ha invocado el comando \`v
 
 A manera de ejemplo, aquí tienes un ejemplo de una respuesta a la solicitud "Qué es RegExp en JavaScript".
 - Fíjate como en el JSON de respuesta, todo es compacto. No hay saltos de línea a excepción de los caracteres '\\n'. Así necesitamos que sea tu respuesta también, solo utilizando '\\n' para especificar saltos de línea. De otra manera, el JSON podría ser inprocesable.
+- De paso, fíjate en como cualquier uso de apóstrofes (\"\") está escapado para evitar errores de procesamiento.
 
 \`\`\`json
-{"title":"Expresiones Regulares (RegExp) en JavaScript","body":"Una expresión regular, también conocida como RegExp, es un patrón de búsqueda que se utiliza para realizar búsquedas y reemplazos sobre cadenas de texto. Se usan con mucha frecuencia en programación de lenguajes de scripting, como JavaScript para localizar y manipular patrones de texto.","codeblock":{"language":"js","content":"const numberRegex = /^\\d+$/;\\n//Ejemplo de uso:\\n    const str = '12345';\\n    if (numberRegex.test(str)) {\\n    console.log('La cadena contiene solo números');\\n    } else {\\n        console.log('La cadena contiene caracteres no numéricos');\\n    }"}}
+{"title":"Expresiones Regulares (RegExp) en JavaScript","body":"Una expresión regular, también conocida como RegExp, es un patrón de búsqueda que se utiliza para realizar búsquedas y reemplazos sobre cadenas de texto. Se usan con mucha frecuencia en programación de lenguajes de scripting, como JavaScript para localizar y manipular patrones de texto.","codeblock":{"language":"js","content":"const numberRegex = /^\\d+$/;\\n//Ejemplo de uso:\\n    const str = \\\"12345\\\";\\n    if (numberRegex.test(str)) {\\n    console.log('La cadena contiene solo números');\\n    } else {\\n        console.log('La cadena contiene caracteres no numéricos');\\n    }"}}
 
 La solicitud que se te hizo y para la cual deberás generar un JSON, siguiendo el formato que se te dio anteriormente, es:
 "${split[1]}"
@@ -147,7 +148,7 @@ La solicitud que se te hizo y para la cual deberás generar un JSON, siguiendo e
         botIO.say(
           botIO._msg.author.mention + "\n",
           `**${parsedResponse.title}**` + "\n",
-          parsedResponse.body + "\n",
+          parsedResponse.body,
           `\`\`\`${parsedResponse.codeblock.language}\n${parsedResponse.codeblock.content}\`\`\``
         );
 
